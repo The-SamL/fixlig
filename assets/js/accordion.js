afe861886d90ac37994248603e164eeaa85c5172
mthibault006 <thibault.maxime@outlook.com>
2021-03-03 12:31:10 +0100

    profil + js accordion

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,17 +1,18 @@
 var acc = document.getElementsByClassName("accordion");
-var arrow = document.getElementsByClassName("fab");
 var i;
-
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
+
+        document.getElementById("arrow1").className = "fas fa-sort-up";
+
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
+            document.getElementById("arrow1").className = "fas fa-sort-down";
+
         } else {
             panel.style.maxHeight = panel.scrollHeight + "px";
         }
     });
 }
-
-fa - sort - down
